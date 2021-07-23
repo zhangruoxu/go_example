2da7fbbfedfe4e27674fb5c194023ea7d4a46b44
Yifei Zhang <yifei.zhang1992@outlook.com>
2021-07-23 22:16:18 +0800

    rate limiting

---
+++ rate_limiting/go.mod
@@ -0,0 +1,3 @@
+module yifei.com/rate_limiting
+
+go 1.17
---
+++ rate_limiting/rate_limiting.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+const NumRequest = 5
+
+func testRateLimiting() {
+	requests := make(chan int, NumRequest)
+	limiter := time.Tick(200 * time.Millisecond)
+
+	for i := 0; i < NumRequest; i++ {
+		fmt.Println("Send request", i)
+		requests <- i
+	}
+
+	close(requests)
+
+	for r := range requests {
+		limit := <- limiter
+		fmt.Printf("Request %v limit %v, time %v\n", r, limit, time.Now())
+	}
+}
+
+// ============================================
+
+func testBurstyTimer() {
+	testBurstyTimer := make(chan time.Time, 3)
+	burstyRequests := make(chan int, NumRequest)
+
+	for i := 0; i < 3; i++ {
+		testBurstyTimer <- time.Now()
+	}
+
+	go func() {
+		for t:= range time.Tick(200 * time.Millisecond) {
+			testBurstyTimer <- t
+		}
+	}()
+
+	for i := 0; i < NumRequest; i++ {
+		burstyRequests <- i
+	}
+	
+	close(burstyRequests)
+
+	for r := range burstyRequests {
+		limit := <- testBurstyTimer
+		fmt.Printf("Request %v limit %v, time %v\n", r, limit, time.Now())
+	}
+}
+
+func main() {
+	// testRateLimiting()
+	testBurstyTimer()
+}
